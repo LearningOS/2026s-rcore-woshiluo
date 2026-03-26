4f60bd64ef9f69248d84f0ae43e85fab6fe199b4
Woshiluo Luo <woshiluo.luo@outlook.com>
2026-03-26 13:32:07 +0800

    ci: use latest toolchains
    Signed-off-by: Woshiluo Luo <woshiluo.luo@outlook.com>

--- os/src/sbi.rs
+++ os/src/sbi.rs
@@ -2,13 +2,13 @@
 
 use core::arch::asm;
 
-const SBI_SET_TIMER: usize = 0;
+const SBI_SET_TIMER: usize = 0x54494D45;
 const SBI_CONSOLE_PUTCHAR: usize = 1;
-const SBI_SHUTDOWN: usize = 8;
+const SBI_SHUTDOWN: usize = 0x53525354;
 
 /// general sbi call
 #[inline(always)]
-fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
+fn sbi_call(which: usize, fid: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
     let mut ret;
     unsafe {
         asm!(
@@ -16,7 +16,7 @@ fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
             inlateout("x10") arg0 => ret,
             in("x11") arg1,
             in("x12") arg2,
-            in("x16") 0,
+            in("x16") fid,
             in("x17") which,
         );
     }
@@ -25,16 +25,16 @@ fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
 
 /// use sbi call to set timer
 pub fn set_timer(timer: usize) {
-    sbi_call(SBI_SET_TIMER, timer, 0, 0);
+    sbi_call(SBI_SET_TIMER, 0, timer, 0, 0);
 }
 
 /// use sbi call to putchar in console (qemu uart handler)
 pub fn console_putchar(c: usize) {
-    sbi_call(SBI_CONSOLE_PUTCHAR, c, 0, 0);
+    sbi_call(SBI_CONSOLE_PUTCHAR, 0, c, 0, 0);
 }
 
 /// use sbi call to shutdown the kernel
 pub fn shutdown() -> ! {
-    sbi_call(SBI_SHUTDOWN, 0, 0, 0);
+    sbi_call(SBI_SHUTDOWN, 0, 0, 0, 0);
     panic!("It should shutdown!");
 }
--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -54,7 +54,7 @@ lazy_static! {
         let mut tasks = [TaskControlBlock {
             task_cx: TaskContext::zero_init(),
             task_status: TaskStatus::UnInit,
-            syscall_counter: [0; _]
+            syscall_counter: [0; crate::syscall::MAX_SYSCALL_ID]
         }; MAX_APP_NUM];
         for (i, task) in tasks.iter_mut().enumerate() {
             task.task_cx = TaskContext::goto_restore(init_app_cx(i));
